51097874f8ff9e3ae4c02c6995fe1872da37bb27
DoubleSang <hdsa1564@gmail.com>
2026-05-15 10:45:16 +0900

    015

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -11,10 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,17 +23,18 @@ public class PostController {
     private final PostService postService;
 
     private String getWriteFormHtml() {
-        return getWriteFormHtml("", "", "", "");
+        return getWriteFormHtml("", "", "");
     }
 
     private String getWriteFormHtml(
-            String errorFieldName,
             String errorMessage,
             String title,
             String content
     ) {
         return """
-                <div style="color: red;">%s</div>
+                <ul style="color: red;">
+                    %s
+                </ul>
                 
                 <form method="POST" action="doWrite">
                   <input type="text" name="title" placeholder="제목" value="%s" autofocus>
@@ -43,19 +45,19 @@ public class PostController {
                 </form>
                 
                 <script>
-                const errorFieldName = '%s';
+                // 현재까지 나온 모든 폼 검색
+                const forms = document.querySelectorAll('form');
+                // 그 중에서 가장 마지막 폼 1개 찾기
+                const lastForm = forms[forms.length - 1];
+                
+                const errorFieldName = lastForm.previousElementSibling?.querySelector('li')?.dataset?.errorFieldName || '';
                 
                 if ( errorFieldName.length > 0 )
                 {
-                    // 현재까지 나온 모든 폼 검색
-                    const forms = document.querySelectorAll('form');
-                    // 그 중에서 가장 마지막 폼 1개 찾기
-                    const lastForm = forms[forms.length - 1];
-                
                     lastForm[errorFieldName].focus();
                 }
                 </script>
-                """.formatted(errorMessage, title, content, errorFieldName);
+                """.formatted(errorMessage, title, content);
     }
 
     @GetMapping("/posts/write")
@@ -68,11 +70,11 @@ public class PostController {
     @AllArgsConstructor
     @Getter
     public static class WriteForm {
-        @NotBlank(message = "제목을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "제목은 2자 이상, 20자 이하로 입력가능합니다.")
+        @NotBlank(message = "1-제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "2-제목은 2자 이상, 20자 이하로 입력가능합니다.")
         private String title;
-        @NotBlank(message = "내용을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "내용은 2자 이상, 20자 이하로 입력가능합니다.")
+        @NotBlank(message = "3-내용을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "4-내용은 2자 이상, 20자 이하로 입력가능합니다.")
         private String content;
     }
 
@@ -84,12 +86,16 @@ public class PostController {
             BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) {
-            FieldError fieldError = bindingResult.getFieldError();
+            String errorFieldName = "title";
+            String errorMessage = bindingResult
+                    .getFieldErrors()
+                    .stream()
+                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-", 3))
+                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1], field[0], field[2]))
+                    .sorted()
+                    .collect(Collectors.joining("\n"));
 
-            String errorFieldName = fieldError.getField();
-            String errorMessage = fieldError.getDefaultMessage();
-
-            return getWriteFormHtml(errorFieldName, errorMessage, form.getTitle(), form.getContent());
+            return getWriteFormHtml(errorMessage, form.getTitle(), form.getContent());
         }
 
         Post post = postService.write(form.getTitle(), form.getContent());
